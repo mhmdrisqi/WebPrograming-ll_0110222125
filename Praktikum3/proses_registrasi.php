8c84a3fcb20cd766d01c7a9ab586ecc3624b0612
mhmdrisqi <risqi.muhamad1912@gmail.com>
2023-03-07 15:44:08 +0700

    upload praktikum3

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,21 @@
+<?php 
+$domisili = ["Jakarta","Bogor", "Depok", "Tanggerang","Bekasi"];
+
+$program_studi = [
+    "TI"=> "Teknik Informatika",
+    "SI" => "Sistem Infomasi",
+    "BD" => "Bisnis Digital",
+
+];
+
+$skils = [
+    "HTML <br>" => 10,
+    "CSS <br>" => 10,
+    "Javascript <br>" => 20,
+    "RWD Bootstrap <br>" => 20,
+    "PHP <br>" => 30,
+    "Python <br>" => 30,
+    "Java" => 50
+];
+
+?>
